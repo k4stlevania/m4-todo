520534f25a81c51345d4e74bf783fc661cb76244
francisco <frncisco1243@gmail.com>
2026-07-07 08:08:09 -0400

    added navbar to index

--- assets/js/Task.js
+++ assets/js/Task.js
@@ -1,10 +1,18 @@
-
+let tasks = [];
 class Task{
-    constructor(description, state){
+    constructor(description){
         // i'll change this later
         this.id = Math.random() * 100
         this.description = description;
         this.state = state;
         this.creationDate = new Date();
+    }
+
+    changeState(){
+        return null;
+    }
+
+    editTask(){
+        return null;
     }
 }
